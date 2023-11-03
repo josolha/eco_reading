3dfdbacd5809e4b72599c2507fe84f8e05dc1b3e
sim <sgi032151@gmail.com>
2023-11-03 17:08:25 +0900

    feat: naver book api 적용

--- src/main/java/com/checkcheck/ecoreading/domain/boards/controller/BoardApiController.java
+++ src/main/java/com/checkcheck/ecoreading/domain/boards/controller/BoardApiController.java
@@ -1,21 +1,29 @@
 package com.checkcheck.ecoreading.domain.boards.controller;
 
+import com.checkcheck.ecoreading.domain.boards.dto.BookDTO;
 import com.checkcheck.ecoreading.domain.boards.service.BoardService;
+import com.checkcheck.ecoreading.domain.boards.service.BookService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardApiController {
 
     private final BoardService boardService;
-    @PostMapping("/board/add")
+    private final BookService bookService;
+
+    @PostMapping("/board/new")
     public String addImage(@RequestParam("image") MultipartFile multipartFile) throws IOException {
         System.out.println(multipartFile.toString());
         boardService.saveFile(multipartFile);
@@ -23,5 +31,19 @@ public class BoardApiController {
         //todo: db에 url 저장
 
         return null;
+    }
+
+    @GetMapping("/board/bookSearch")
+    public String search(@RequestParam String text, Model model) {
+        List<BookDTO> books = bookService.searchBooks(text);
+        System.out.println("검색결과: "+ books);
+        model.addAttribute("books", books);
+        return "/content/user/bookSearch";
+    }
+
+    @PostMapping("/board/bookSearch")
+    public String fillBook(BookDTO bookDTO){
+        System.out.println("북디티오: "+bookDTO);
+        return "/content/user/boardAddForm";
     }
 }
---
+++ src/main/java/com/checkcheck/ecoreading/domain/boards/service/BookService.java
@@ -0,0 +1,77 @@
+package com.checkcheck.ecoreading.domain.boards.service;
+
+import com.checkcheck.ecoreading.domain.boards.dto.BookDTO;
+import com.checkcheck.ecoreading.domain.boards.dto.NaverResultDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class BookService {
+
+    @Value("${naver-property.clientId}")
+    private String naverClientId;
+
+    @Value("${naver-property.clientSecret}")
+    private String naverClientSecret;
+
+    //네이버 검색 API 요청
+    public List<BookDTO> searchBooks (String text) {
+        // JSON 결과
+        // String apiURL = "https://openapi.naver.com/v1/search/book.json?query="+ text;
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/book.json")
+                .queryParam("query", text)
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "sim")
+                .encode()
+                .build()
+                .toUri();
+
+//        log.info("uri: "+uri);
+
+        // Spring 요청 제공 클래스  (Request)
+        RequestEntity<Void> request = RequestEntity.get(uri)
+                .header("X-Naver-Client-Id", naverClientId)
+                .header("X-Naver-Client-Secret", naverClientSecret)
+                .build();
+
+        // Spring 제공 restTemplate (Response)
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+
+        // JSON 파싱 (JSON 문자열을 객체로 만듦, 문서화하기)
+        ObjectMapper om = new ObjectMapper();
+        NaverResultDTO resultDTO = null;
+
+        try {
+            resultDTO = om.readValue(response.getBody(), NaverResultDTO.class);
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        List<BookDTO> books = resultDTO.getItems();
+//        BookDTO bookDTO = new BookDTO();
+//        bookDTO.getIsbn();
+        return books;
+    }
+}
