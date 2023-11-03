d0252e4d7ccbc7467a4d46424dab4d35fcc8beda
[yubin-im] <[ybyb130@gmail.com]>
2023-11-03 17:12:54 +0900

    feat: main, detail 생성

--- src/main/java/com/checkcheck/ecoreading/domain/boards/controller/BoardViewController.java
+++ src/main/java/com/checkcheck/ecoreading/domain/boards/controller/BoardViewController.java
@@ -1,4 +1,21 @@
 package com.checkcheck.ecoreading.domain.boards.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
 public class BoardViewController {
+    @GetMapping("/new")
+    public String addBoard() {
+        return "/content/user/boardAddForm";
+    }
+
+    @GetMapping("/board/detail")
+    public String mainhtml(){
+        return "/content/board/detail";
+    }
 }
