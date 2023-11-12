29fa8f5bd5d8c84ba44982f13a9782722ffb04ca
josolha <josolha@nate.com>
2023-11-12 18:32:51 +0900

    fix : 병합에러 수정

--- src/main/java/com/checkcheck/ecoreading/domain/books/repository/BookRepository.java
+++ src/main/java/com/checkcheck/ecoreading/domain/books/repository/BookRepository.java
@@ -1,12 +1,9 @@
 package com.checkcheck.ecoreading.domain.books.repository;
 
-import com.checkcheck.ecoreading.domain.boards.dto.InsertBookDTO;
-import com.checkcheck.ecoreading.domain.boards.entity.Boards;
 import com.checkcheck.ecoreading.domain.books.entity.Books;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Books, Long> {
